a52ed166d4b20ca2c794ff171f0562dcd2485ded
meageropoulos <unfoldingpast@gmail.com>
2025-06-14 12:35:21 +0000

    updated model path

--- inference_realesrgan_video.py
+++ inference_realesrgan_video.py
@@ -176,7 +176,7 @@ def inference_video(args, video_save_path, device=None, total_workers=1, worker_
     if args.model_name == 'RealESRGAN_x4plus':  # x4 RRDBNet model
         model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
         netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth']
+        file_url = ['https://github.com/meageropoulos/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth']
     elif args.model_name == 'RealESRNet_x4plus':  # x4 RRDBNet model
         model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
         netscale = 4
